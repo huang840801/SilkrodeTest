e4b0c2b5d3f95ef43f97c1aa3be065141648578f
GuanHong <david840801@gmail.com>
2021-07-24 17:27:19 +0800

    complete user page and mine page

---
+++ app/src/main/java/com/guanhong/silkrodetest/view/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.guanhong.silkrodetest.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.guanhong.silkrodetest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userFragment = UserFragment()
+        val infoFragment = InfoFragment()
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+
+        viewPager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager) {
+
+            override fun getCount() = 2
+            override fun getItem(position: Int): Fragment {
+
+                return when (position) {
+                    0 -> userFragment
+                    1 -> infoFragment
+                    else -> throw Exception()
+                }
+            }
+        }
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.icon_user -> {
+
+                    viewPager.currentItem = 0
+                }
+                R.id.icon_info -> {
+
+                    viewPager.currentItem = 1
+                }
+            }
+
+            true
+        }
+
+    }
+}
